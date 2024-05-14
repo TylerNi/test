03bb3489365632ea8ccdc492f754b24f9028789a
Victor Morais <vikmo16@gmail.com>
2024-05-14 17:19:36 -0400

    Choix Menu2 + PArtie et main

--- TP2/TP2/src/ca/qc/bdeb/sim202/tp2/Partie.java
+++ TP2/TP2/src/ca/qc/bdeb/sim202/tp2/Partie.java
@@ -2,18 +2,19 @@ package ca.qc.bdeb.sim202.tp2;
 
 import java.io.Serializable;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Partie implements Serializable {
 
     private PlateauDeJeu plateauDeJeu;
 
-    private LinkedList<Joueur> listJoueur;
+    private LinkedList<Joueur> listeJoueur;
 
     private int indicateur;
 
-    public Partie(PlateauDeJeu plateauDeJeu, LinkedList<Joueur> listJoueur, int indicateur) {
+    public Partie(PlateauDeJeu plateauDeJeu, LinkedList<Joueur> listeJoueur, int indicateur) {
         this.plateauDeJeu = plateauDeJeu;
-        this.listJoueur = listJoueur;
+        this.listeJoueur = listeJoueur;
         this.indicateur = indicateur;
     }
 
@@ -26,7 +27,43 @@ public class Partie implements Serializable {
             System.out.println("\033[34m" + liste[i] +"\033[39m");
         }
 
+        {
+            Joueur joueurActuel;
+
+            for (Joueur j : listeJoueur) {
+                if (j.isFaillite()) {
+                    System.out.println("\033[96m" + j.getNom() + " a fait faillite \033[39m");
+                } else {
+                    System.out.println("\033[96m" + j.getNom() + "est sur la case : " + liste[j.getPosition()].getNomCase() + " et possède " + j.getNombreArgent() + "$.\033[39m");
+                }
+            }
+
+            joueurActuel = listeJoueur.poll();
+            listeJoueur.add(joueurActuel);
+
+            System.out.println("\033[96m" + "C'est au tour de " +joueurActuel + "\033[35m");
+
+
+        }
     }
 
+    public static ChoixMenuDansPartie menu() {
+        Scanner sc = new Scanner(System.in);
+        String choix;
+
+        System.out.println("""
+                1) Charger la partie de sauvegarde\s
+                2) Commencer une nouvelle partie\s
+                3) Quitter""");
+        System.out.print("Faites votre choix (1,3): ");
+        choix = sc.nextLine();
+
+        while (!choix.equals("1") && !choix.equals("2") && !choix.equals("3")) {
+            System.out.println("Le choix est invalide, il doit etre de 1, 2 ou 3");
+            System.out.print("Faites votre choix (1,3): ");
+            choix = sc.nextLine();
+        }
+        return ChoixMenuDansPartie.getValeurAvecIndice(choix);
+    }
 
 }
