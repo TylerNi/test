3eb07ca8700ec14534eefdfcaa5df50a979fad03
TylerNi <nichols.tyler13@gmail.com>
2024-05-14 17:11:40 -0400

    Finition de ServicePublic et Terrain

--- TP2/TP2/src/ca/qc/bdeb/sim202/tp2/Terrain.java
+++ TP2/TP2/src/ca/qc/bdeb/sim202/tp2/Terrain.java
@@ -15,14 +15,23 @@ public class Terrain extends CasePropriete{
     @Override
     void faireAction(Joueur joueur) {
         if(getProprietaire() == null){ //S'il n'y a pas de propriétaire, le joueur l'achète.
-            setProprietaire(joueur);
-            joueur.setNombreArgent(joueur.getNombreArgent() - getPrixAchat());
+            if (joueur.getNombreArgent() >= getPrixAchat()) {
+                setProprietaire(joueur);
+                joueur.setNombreArgent(joueur.getNombreArgent() - getPrixAchat());
+                System.out.println(joueur.getNom() + "a acheté " + nomCase + "pour " + this.getPrixAchat() + ". " + joueur.getNombreArgent() + "$ restant.");
+            }else {
+                System.out.println(joueur.getNom() + "n'a pas assez d'argent, il ne peut donc pas acheter " + nomCase);
+            }
 
         }else if(getProprietaire().getNom().equals(joueur.getNom())){ //S'il y a un propriétaire autre que lui même,
             joueur.setNombreArgent(joueur.getNombreArgent() - getLoyer()); //le joueur paye le loyer au propriétaire.
             getProprietaire().setNombreArgent(getProprietaire().getNombreArgent() + getLoyer());
+            System.out.println(joueur.getNom() + " a payé " + this.getLoyer() + "$ a " + getProprietaire() + ".");
+            System.out.println(joueur.getNom() + " a maintenant " + joueur.getNombreArgent() + "$ et " + getProprietaire() + getProprietaire().getNombreArgent() + "$");
 
-        }//Si le joueur est propriétaire, rien.
+        }else {
+            System.out.println(joueur.getNom() + "est propriétaire de " + nomCase + ". Il a" + joueur.getNombreArgent() + "$");
+        }
     }
 
     @Override
